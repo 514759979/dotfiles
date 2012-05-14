bc5b82d89192f6a6491cecd9449a1324f954404e
osily <ly50247@gmail.com>
2012-05-14 13:21:03 +0800

    add Qmmp

--- .dwm/config.h
+++ .dwm/config.h
@@ -27,6 +27,7 @@ static const Rule rules[] = {
     /* class      instance    title       tags mask     isfloating   monitor */
 //    { "Gimp",      NULL,       NULL,       0,           True,           -1 },
     { "Yad",       NULL,       NULL,       0,           True,           -1 },
+    { "Qmmp",       NULL,       NULL,       0,           True,           -1 },
     { "MPlayer",   NULL,       NULL,       0,           True,           -1 },
     { "Thunderbird",  NULL,       NULL,       2,           False,           -1 },
     { "Chromium",  NULL,       NULL,       1,           False,           -1 },
