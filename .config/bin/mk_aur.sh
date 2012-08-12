b7399ba6d15fa24e03afe086060aab4649a79c4f
osily <ly50247@gmail.com>
2012-08-12 19:42:48 +0800

    Remove useless execute file.

--- .config/bin/mk_aur.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-all=34423
-page=$[all/250+1]
-
-echo -n > result.htm
-for i in {0..$page}; do
-    curl "https://aur.archlinux.org/packages.php?O=$[i*250]&PP=250" >> result.htm
-done
-cat result.htm | grep "'packages.php?ID" | sed -e "s/^.*<span class='black'>//g" -e "s/<\/.*$//g" > result.txt
--- .config/templete/cgi.cgi
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Content-type:text/html\n"
-echo '<title>aa</title>'
-#echo $QUERY_STRING
