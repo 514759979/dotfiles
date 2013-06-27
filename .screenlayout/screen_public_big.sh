cc8d4605bfef30085ce4737d41960a5dbedce09d
osily <ly50247@126.com>
2013-06-27 10:07:46 +0800

    update

--- .screenlayout/screen_public_big.sh
+++ .screenlayout/screen_public_big.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 xrandr --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 0x1050 --rotate normal --output VGA1 --mode 1400x1050 --pos 0x0 --rotate normal
+habak ~/.jpg
