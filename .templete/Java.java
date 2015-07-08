d66dacc3cbfcb37298ce1c057af369f013532af1
Gore Liu <goreliu@126.com>
2015-07-08 18:19:37 +0800

    Java.java

--- .templete/Java.java
+++ .templete/Java.java
@@ -3,7 +3,7 @@ import java.math.*;
 import java.util.*;
 
 class Main {
-  public static void main(String args[]) {
-    Scanner cin = new Scanner(System.in);
-  }
+    public static void main(String args[]) {
+        Scanner cin = new Scanner(System.in);
+    }
 }
