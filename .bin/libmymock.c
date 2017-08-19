83ee1e3eacc14e97e86e671de07535e57762adfe
Gore Liu <goreliu@126.com>
2017-08-19 13:22:42 +0800

    Add .bin/libmymock.c

---
+++ .bin/libmymock.c
@@ -0,0 +1,11 @@
+int setrlimit(int resource, const struct rlimit *rlp) {
+    return 0;
+}
+
+int getrlimit(int resource, struct rlimit *rlp) {
+    return 0;
+}
+
+int nice(int incr) {
+    return 0;
+}
