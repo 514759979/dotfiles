c8328b7d9a75eb2135bf76b00b16ff11f534d620
Gore Liu <goreliu@126.com>
2017-09-22 10:44:43 +0800

    update

---
+++ .bin/src/cut.js
@@ -0,0 +1,21 @@
+var cut_msg = ""
+
+function cut_begin() {
+    cut_msg += mp.get_property("time-pos")
+}
+
+function cut_end() {
+    var filename = mp.get_property("filename")
+    cut_msg += " " + mp.get_property("time-pos") + "\n"
+	mp.utils.write_file("file://" + filename + ".time.txt", cut_msg)
+}
+
+function cut_run() {
+    var filename = mp.get_property("filename")
+	mp.utils.write_file("file://cut_run.sh", "~/.bin/makevideo " + filename + " " + filename + ".time.txt")
+    mp.utils.subprocess({"args":["run-wsl-file", "cut_run.sh"]})
+}
+
+mp.add_key_binding("i", "cut_begin", cut_begin)
+mp.add_key_binding("n", "cut_end", cut_end)
+mp.add_key_binding("c", "cut_run", cut_run)
